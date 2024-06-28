bdd568600025cd4e6fe6b06defc35766726a5597
Kosall Nim <kosall123nim@gmail.com>
2024-06-28 19:43:55 +0700

    add MyBulidingProjectApplication.java to staging area

---
+++ src/main/java/org/kosal/my_buliding_project/MyBulidingProjectApplication.java
@@ -0,0 +1,13 @@
+package org.kosal.my_buliding_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBulidingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBulidingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kosal/my_buliding_project/MyBulidingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kosal.my_buliding_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBulidingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
